0ed0ae145f2a2697117875cba08b03e4f0d2a7be
vatsan47 <89676340+vatsan47@users.noreply.github.com>
2021-09-26 22:04:27 +0530

    Added axios and cheerio code (Part 2)

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ let express = require("express");
 let PORT = process.env.PORT || 4000;
 let app = express();
 let path = require('path');
+
+var axios = require('axios');
+var cheerio = require('cheerio');
+
 // Configure middleware
 // Parse request body as JSON
 app.use(express.urlencoded({ extended: true }));
@@ -13,6 +17,25 @@ app.use(express.static("public"));
 app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname + "./public/index.html"));
 })
+
+var scrape = function(searchTerm) {
+    var idioms = [];
+    return axios.get("https://idioms.thefreedictionary.com/" + searchTerm).then(function(response) {
+   
+        var $ = cheerio.load(response.data);
+       
+          var listItems = $("ul.idiKw li a").each(function(i, elem) {
+            let obj = {
+                idiom: $(elem).text(),
+                link: "https://thefreedictionary.com/" + $(elem).attr("href")
+            };
+            idioms.push(obj);
+        });
+  
+        return idioms;
+    });
+     console.log(idioms);
+  }
 
 app.listen(PORT, function() {
     console.log("App listening on port " + PORT);
